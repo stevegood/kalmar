607ff48655e58f1e602f3ec897292e9caf7dd34a
Steve Good <stephen.good@umusic.com>
2021-12-11 15:52:16 -0800

    initial wiring up of graphql starting server

--- cmd/serve.go
+++ cmd/serve.go
@@ -16,9 +16,8 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
+	"github.com/stevegood/kalmar/pkg/server"
 )
 
 // serveCmd represents the serve command
@@ -32,7 +31,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("serve called")
+		server.Exec()
 	},
 }
 
---
+++ pkg/server/server.go
@@ -0,0 +1,29 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/stevegood/kalmar/pkg/server/graph"
+	"github.com/stevegood/kalmar/pkg/server/graph/generated"
+)
+
+const defaultPort = "8080"
+
+func Exec() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
